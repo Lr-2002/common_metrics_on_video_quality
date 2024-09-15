89807f956f58d6c5188f2fcb06f86cdd8714e38e
zhouziheng <josephziheng@gmail.com>
2024-09-15 23:10:47 +0800

    feat: change to class

--- calculate_fvd.py
+++ calculate_fvd.py
@@ -2,65 +2,65 @@ import numpy as np
 import torch
 from tqdm import tqdm
 
-def trans(x):
-    # if greyscale images add channel
-    if x.shape[-3] == 1:
-        x = x.repeat(1, 1, 3, 1, 1)
+class FVD:
+    def __init__(self, method='styleganv', device='cuda'):
+        if method == 'styleganv':
+            from .fvd.styleganv.fvd import get_fvd_feats, frechet_distance, load_i3d_pretrained
+        elif method == 'videogpt':
+            from .fvd.videogpt.fvd import load_i3d_pretrained
+            from .fvd.videogpt.fvd import get_fvd_logits as get_fvd_feats
+            from .fvd.videogpt.fvd import frechet_distance
+        self.get_fvd_feats = get_fvd_feats
+        self.frechet_distance = frechet_distance
 
-    # permute BTCHW -> BCTHW
-    x = x.permute(0, 2, 1, 3, 4) 
+        self.i3d = load_i3d_pretrained(device=device)
+        self.device = device
 
-    return x
+    def trans(self, x):
+        # if greyscale images add channel
+        if x.shape[-3] == 1:
+            x = x.repeat(1, 1, 3, 1, 1)
 
-def calculate_fvd(videos1, videos2, device, method='styleganv'):
+        # permute BTCHW -> BCTHW
+        x = x.permute(0, 2, 1, 3, 4)
 
-    if method == 'styleganv':
-        from fvd.styleganv.fvd import get_fvd_feats, frechet_distance, load_i3d_pretrained
-    elif method == 'videogpt':
-        from fvd.videogpt.fvd import load_i3d_pretrained
-        from fvd.videogpt.fvd import get_fvd_logits as get_fvd_feats
-        from fvd.videogpt.fvd import frechet_distance
+        return x
+    def __call__(self, input1, input2):
+        return self.calculate_fvd(input1, input2)
 
-    print("calculate_fvd...")
+    def calculate_fvd(self, videos1, videos2):
+        # videos [batch_size, timestamps, channel, h, w]
 
-    # videos [batch_size, timestamps, channel, h, w]
-    
-    assert videos1.shape == videos2.shape
+        assert videos1.shape == videos2.shape
 
-    i3d = load_i3d_pretrained(device=device)
-    fvd_results = []
+        fvd_results = []
 
-    # support grayscale input, if grayscale -> channel*3
-    # BTCHW -> BCTHW
-    # videos -> [batch_size, channel, timestamps, h, w]
+        # support grayscale input, if grayscale -> channel*3
+        # BTCHW -> BCTHW
+        # videos -> [batch_size, channel, timestamps, h, w]
 
-    videos1 = trans(videos1)
-    videos2 = trans(videos2)
+        videos1 = self.trans(videos1)
+        videos2 = self.trans(videos2)
 
-    fvd_results = {}
+        fvd_results = {}
 
-    # for calculate FVD, each clip_timestamp must >= 10
-    for clip_timestamp in tqdm(range(10, videos1.shape[-3]+1)):
-       
-        # get a video clip
-        # videos_clip [batch_size, channel, timestamps[:clip], h, w]
-        videos_clip1 = videos1[:, :, : clip_timestamp]
-        videos_clip2 = videos2[:, :, : clip_timestamp]
+        # for calculate FVD, each clip_timestamp must >= 10
+        for clip_timestamp in tqdm(range(10, videos1.shape[-3]+1)):
 
-        # get FVD features
-        feats1 = get_fvd_feats(videos_clip1, i3d=i3d, device=device)
-        feats2 = get_fvd_feats(videos_clip2, i3d=i3d, device=device)
-      
-        # calculate FVD when timestamps[:clip]
-        fvd_results[clip_timestamp] = frechet_distance(feats1, feats2)
+            # get a video clip
+            # videos_clip [batch_size, channel, timestamps[:clip], h, w]
+            videos_clip1 = videos1[:, :, : clip_timestamp]
+            videos_clip2 = videos2[:, :, : clip_timestamp]
 
-    result = {
-        "value": fvd_results,
-        "video_setting": videos1.shape,
-        "video_setting_name": "batch_size, channel, time, heigth, width",
-    }
+            # get FVD features
+            feats1 = self.get_fvd_feats(videos_clip1, i3d=self.i3d, device=self.device)
+            feats2 = self.get_fvd_feats(videos_clip2, i3d=self.i3d, device=self.device)
 
-    return result
+            # calculate FVD when timestamps[:clip]
+            fvd_results[clip_timestamp] = self.frechet_distance(feats1, feats2)
+        result = fvd_results
+
+        return result
 
 # test code / using example
 
